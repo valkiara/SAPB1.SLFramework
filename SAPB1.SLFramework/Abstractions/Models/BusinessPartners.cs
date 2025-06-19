91b4974e3863ec5d202ed4389b96475a7c564204
Beka Latsabidze <blatsabidze@outlook.com>
2025-06-19 12:15:04 +0400

    Add BPBankAccount class and enhance BusinessPartners model
    Introduced a new `BPBankAccount` class to manage bank account
    details, with required properties like `BankCode` and `AccountNo`.
    Updated the `BusinessPartners` class to include a `BPBankAccounts`
    property, initialized as an empty list in a new parameterless
    constructor. These changes improve handling of bank account
    information for business partners.

--- SAPB1.SLFramework/Abstractions/Models/BusinessPartners.cs
+++ SAPB1.SLFramework/Abstractions/Models/BusinessPartners.cs
@@ -11,5 +11,50 @@ namespace SAPB1.SLFramework.Abstractions.Models
         public string? Phone1 { get; set; }
         public string? IBAN { get; set; }
         public string? DefaultBankCode { get; set; }
+
+        public IList<BPBankAccount> BPBankAccounts { get; set; }
+
+        public BusinessPartners()
+        {
+            BPBankAccounts = [];
+        }
+    }
+
+    public class BPBankAccount
+    {
+        public int? LogInstance { get; set; }
+        public string? UserNo4 { get; set; }
+        public string? BPCode { get; set; }
+        public string? County { get; set; }
+        public string? State { get; set; }
+        public string? UserNo2 { get; set; }
+        public string? IBAN { get; set; }
+        public string? ZipCode { get; set; }
+        public string? City { get; set; }
+        public string? Block { get; set; }
+        public string? Branch { get; set; }
+        public string? Country { get; set; }
+        public string? Street { get; set; }
+        public string? ControlKey { get; set; }
+        public string? UserNo3 { get; set; }
+        public required string BankCode { get; set; }
+        public required string AccountNo { get; set; }
+        public string? UserNo1 { get; set; }
+        public int? InternalKey { get; set; }
+        public string? BuildingFloorRoom { get; set; }
+        public string? BIK { get; set; }
+        public string? AccountName { get; set; }
+        public string? CorrespondentAccount { get; set; }
+        public string? Phone { get; set; }
+        public string? Fax { get; set; }
+        public string? CustomerIdNumber { get; set; }
+        public string? ISRBillerID { get; set; }
+        public int? ISRType { get; set; }
+        public string? BICSwiftCode { get; set; }
+        public string? ABARoutingNumber { get; set; }
+        public string? MandateID { get; set; }
+        public DateTimeOffset? SignatureDate { get; set; }
+        public DateTimeOffset? MandateExpDate { get; set; }
+        public int? SEPASeqType { get; set; }
     }
 }
