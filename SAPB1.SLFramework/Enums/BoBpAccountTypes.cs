4de06f34ab8f744d045e06736f70d5afded556be
Beka Latsabidze <blatsabidze@outlook.com>
2025-06-20 15:30:18 +0400

    Refactor enum member casing to camelCase
    Updated the `BoBpAccountTypes` enum in the `SAPB1.SLFramework.Enums`
    namespace to use camelCase for all member names. Previously, the
    members were in PascalCase (e.g., `Bpat_General`), and they are now
    in camelCase (e.g., `bpat_General`) for consistency.

--- SAPB1.SLFramework/Enums/BoBpAccountTypes.cs
+++ SAPB1.SLFramework/Enums/BoBpAccountTypes.cs
@@ -5,21 +5,21 @@ namespace SAPB1.SLFramework.Enums
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum BoBpAccountTypes
     {
-        Bpat_General = 0,
-        Bpat_DownPayment = 1,
-        Bpat_AssetsAccount = 2,
-        Bpat_Receivable = 3,
-        Bpat_Payable = 4,
-        Bpat_OnCollection = 5,
-        Bpat_Presentation = 6,
-        Bpat_AssetsPayable = 7,
-        Bpat_Discounted = 8,
-        Bpat_Unpaid = 9,
-        Bpat_OpenDebts = 10,
-        Bpat_Domestic = 11,
-        Bpat_Foreign = 12,
-        Bpat_CashDiscountInterim = 13,
-        Bpat_ExchangeRateInterim = 14
+        bpat_General = 0,
+        bpat_DownPayment = 1,
+        bpat_AssetsAccount = 2,
+        bpat_Receivable = 3,
+        bpat_Payable = 4,
+        bpat_OnCollection = 5,
+        bpat_Presentation = 6,
+        bpat_AssetsPayable = 7,
+        bpat_Discounted = 8,
+        bpat_Unpaid = 9,
+        bpat_OpenDebts = 10,
+        bpat_Domestic = 11,
+        bpat_Foreign = 12,
+        bpat_CashDiscountInterim = 13,
+        bpat_ExchangeRateInterim = 14
     }
 
 }
