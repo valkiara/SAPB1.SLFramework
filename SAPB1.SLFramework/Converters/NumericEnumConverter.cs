7aeee9f46345fbabddf48fe0c2d0b95cc7f428ea
Beka Latsabidze <blatsabidze@outlook.com>
2025-07-03 17:56:01 +0400

    Add NumericEnumConverter for JSON enum serialization
    Introduce a new class `NumericEnumConverter<T>` that extends `JsonConverter<T>`. This class facilitates the conversion of numeric values to and from enum types during JSON serialization. It includes overridden methods `Read` and `Write` to handle the reading of numeric values as enums and writing of enum values as their numeric representations, respectively.

---
+++ SAPB1.SLFramework/Converters/NumericEnumConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SAPB1.SLFramework.Converters
+{
+    public class NumericEnumConverter<T> : JsonConverter<T>
+    where T : struct, Enum
+    {
+        public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Number
+                && reader.TryGetInt32(out var i))
+                return (T)Enum.ToObject(typeToConvert, i);
+            throw new JsonException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            writer.WriteNumberValue(Convert.ToInt32(value));
+        }
+    }
+}
