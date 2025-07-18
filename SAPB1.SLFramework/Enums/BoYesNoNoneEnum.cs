1597a5afff05b5c5c638781bb98e8e8398f219fb
Beka Latsabidze <blatsabidze@outlook.com>
2025-07-18 13:47:37 +0400

    BoYesNoNoneEnum

---
+++ SAPB1.SLFramework/Enums/BoYesNoNoneEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SAPB1.SLFramework.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum BoYesNoNoneEnum
+    {
+        boNO = 0,
+        boYES = 1,
+        boNONE = 2
+    }
+}
