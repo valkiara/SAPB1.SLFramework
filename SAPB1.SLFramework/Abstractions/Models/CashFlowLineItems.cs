e7e5afcc8ed9c9ab2230c6d9f6181e5e406c7d19
Beka Latsabidze <blatsabidze@outlook.com>
2025-08-14 17:22:44 +0400

    cashflowline items

---
+++ SAPB1.SLFramework/Abstractions/Models/CashFlowLineItems.cs
@@ -0,0 +1,14 @@
+﻿using SAPB1.SLFramework.Enums;
+
+namespace SAPB1.SLFramework.Abstractions.Models
+{
+    public class CashFlowLineItems
+    {
+        public BoYesNoEnum? ActiveLineItem { get; set; }
+        public int? Drawer { get; set; }
+        public int? Level { get; set; }
+        public int LineItemID { get; set; }
+        public int LineItemName { get; set; }
+        public int? ParentArticle { get; set; }
+    }
+}
